9c05b2fc0680ce451e79b5dec1de092b9942d650
aleoncini <andrea.leoncini@gmail.com>
2020-09-22 20:27:52 +0200

    fix to import nine holes only round

--- store/src/main/java/org/ossg/store/importing/H19Parser.java
+++ store/src/main/java/org/ossg/store/importing/H19Parser.java
@@ -14,7 +14,6 @@ import org.jsoup.select.Elements;
 import org.ossg.store.model.DayOfEvent;
 import org.ossg.store.model.Round;
 import org.ossg.store.model.Score;
-import org.ossg.store.model.util.Scorecard;
 
 @ApplicationScoped
 public class H19Parser {
@@ -55,6 +54,10 @@ public class H19Parser {
 
             round.setDayOfEvent(dayOfEvent);
 
+            Elements hivals = mainContainer.getElementsByClass("highlights-value");
+            int holes = Integer.parseInt(hivals.first().text());
+            boolean isNineHole = (holes == 9) ? true : false;
+
             Elements grosses = mainContainer.getElementsByClass("stroke_play_gross_score");
             int gross = Integer.parseInt(grosses.first().text());
             Elements nets = mainContainer.getElementsByClass("stroke_play_net_score");
@@ -73,28 +76,40 @@ public class H19Parser {
             Elements parTDs = rows.get(1).getElementsByTag("td");
             // third row contains strokes
             Elements strTDs = rows.get(2).getElementsByTag("td");
+            // fourth row contains points
+            Elements netTDs = rows.get(3).getElementsByTag("td");
 
             for (int i=0; i<18; i++) {
-                Element div = hcpTDs.get(i);
-                int hcp = Integer.parseInt(div.text());
+                Element hcpTD = hcpTDs.get(i);
+                int hcp = Integer.parseInt(hcpTD.text());
 
-                div = parTDs.get(i);
-                int par = Integer.parseInt(div.text());
+                Element parTD = parTDs.get(i);
+                int par = Integer.parseInt(parTD.text());
 
-                div = strTDs.get(i).getElementsByTag("div").first().getElementsByTag("div").first();
-                int strokes = Integer.parseInt(div.text());
+                if(isNineHole && (i > 8)){
+                    round.setScore((i +1), new Score()
+                        .setHcp(hcp)
+                        .setPar(par)
+                        .setStrokes(0)
+                        .setAdditionalStrokes(0)
+                        .setPoints(0)
+                    );
+                } else {
+                    Element div = strTDs.get(i).getElementsByTag("div").first().getElementsByTag("div").first();
+                    int strokes = Integer.parseInt(div.text());
+    
+                    Element netTD = netTDs.get(i * 2);
+                    int points = Integer.parseInt(netTD.text());
 
-                Scorecard sc = new Scorecard();
-                int additionalStrokes = sc.calculateAdditionalStrokes(phcp, hcp);
-                int points = sc.calculatePoints(par, additionalStrokes, strokes);
-
-                round.setScore((i +1), new Score()
-                                    .setHcp(hcp)
-                                    .setPar(par)
-                                    .setStrokes(strokes)
-                                    .setAdditionalStrokes(additionalStrokes)
-                                    .setPoints(points)
-                );
+                    int additionalStrokes = calculateAdditionalStrokes(par, strokes, points);    
+                    round.setScore((i +1), new Score()
+                                        .setHcp(hcp)
+                                        .setPar(par)
+                                        .setStrokes(strokes)
+                                        .setAdditionalStrokes(additionalStrokes)
+                                        .setPoints(points)
+                    );
+                }
             }
 
 		} catch (IOException e) {
@@ -149,4 +164,18 @@ public class H19Parser {
         Elements divNoRounds = doc.getElementsByClass("no-rounds");
         return (divNoRounds.size() > 0) ? false : true;
     }
+
+    private int calculateAdditionalStrokes(int par, int strokes, int points){
+        if(points > 2){
+            return (strokes - par) + (points - 2) ;
+        }
+        if(points == 2){
+            return strokes - par;
+        }
+        if(points == 1){
+            return (strokes - par) - 1;
+        }
+        return 0;
+    }
+
 }
